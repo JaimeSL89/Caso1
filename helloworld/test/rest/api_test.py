b474bdb9432f152c889e1788140ab3a4ecb61b7e
JaimeSL89 <168648067+JaimeSL89@users.noreply.github.com>
2024-06-02 00:34:24 +0200

    Update api_test.py

--- helloworld/test/rest/api_test.py
+++ helloworld/test/rest/api_test.py
@@ -58,7 +58,7 @@ class TestApi(unittest.TestCase):
         url = f"{BASE_URL}/calc/divide/6/0"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
         self.assertEqual(
-            response.status, 406, f"Error en la petición API a {url}"
+            response.status, "406", f"Error en la petición API a {url}"
         )
      
 
